019b201603285642ddf7a275e03bb7badf31f255
Nipun <nipunsankalana98@gmail.com>
2026-04-26 11:11:55 +0530

    create class Inventory and onr method to add item

---
+++ april/26/oop learning/1/src/Inventory.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> items;
+    public Inventory(){
+        items=new ArrayList<>();
+    }
+    public void addItem(Item item){
+        items.add(item);
+    }
+}
