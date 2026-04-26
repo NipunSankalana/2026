16fda5616ba4e5be7bdd20cadaabd94db7c62d3c
Nipun <nipunsankalana98@gmail.com>
2026-04-26 11:32:50 +0530

    create sub class Fruit from super class Item and create constructor of it

---
+++ april/26/oop learning/1/src/Fruit.java
@@ -0,0 +1,9 @@
+public class Fruit extends Item {
+    private String type;
+
+    public Fruit(String type,String name,int qunatity){
+        super(name,qunatity);
+        this.type=type;
+    }
+
+}
