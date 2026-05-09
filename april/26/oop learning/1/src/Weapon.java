2bc0dc6e3e7896a74f7f40d2f77490056830d1af
Nipun <nipunsankalana98@gmail.com>
2026-05-09 20:29:04 +0530

    create weapon class constructor

--- april/26/oop learning/1/src/Weapon.java
+++ april/26/oop learning/1/src/Weapon.java
@@ -3,6 +3,8 @@ public class Weapon extends Item{
     private String type;
 
     public Weapon(String name,int quantity,int damage,String type){
-
+        super(name,quantity);
+        this.damage=damage;
+        this.type=type;
     }
 }
