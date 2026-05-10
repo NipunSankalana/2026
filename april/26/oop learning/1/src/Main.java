40e88781d7c30dd03f1bcaf2eb8719426d04f12a
Nipun <nipunsankalana98@gmail.com>
2026-05-10 09:49:15 +0530

    change main class according to the changes

--- april/26/oop learning/1/src/Main.java
+++ april/26/oop learning/1/src/Main.java
@@ -1,11 +1,9 @@
 public class Main{
     public static void main(String[] args){
         Inventory inventory=new Inventory();
-        Item item=new Item("Generic Item",10);
         //Fruit fruit=new Fruit("Apple",20,"Fuji");
         //Weapon weapon=new Weapon("Sword",2,75,"Mele");
 
-        inventory.addItem(item);
         inventory.addItem("Apple",20,"Fuji");
         inventory.addItem("Sword",2,75,"Mele");
         inventory.displayInventory();
