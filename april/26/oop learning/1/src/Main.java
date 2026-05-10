09ef146763cbf1585071c21c9555be9c43077f41
Nipun <nipunsankalana98@gmail.com>
2026-05-10 08:54:43 +0530

    change main programme according to changes done to inventory class

--- april/26/oop learning/1/src/Main.java
+++ april/26/oop learning/1/src/Main.java
@@ -2,12 +2,12 @@ public class Main{
     public static void main(String[] args){
         Inventory inventory=new Inventory();
         Item item=new Item("Generic Item",10);
-        Fruit fruit=new Fruit("Apple",20,"Fuji");
-        Weapon weapon=new Weapon("Sword",2,75,"Mele");
+        //Fruit fruit=new Fruit("Apple",20,"Fuji");
+        //Weapon weapon=new Weapon("Sword",2,75,"Mele");
 
         inventory.addItem(item);
-        inventory.addItem(fruit);
-        inventory.addItem(weapon);
+        inventory.addItem("Apple",20,"Fuji");
+        inventory.addItem("Sword",2,75,"Mele");
         inventory.displayInventory();
     }
 }
